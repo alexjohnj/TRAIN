f12b324cc9f54c07e3182a992d512c609bffd76c
Alex Jackson <alex@alexj.org>
2017-03-02 10:24:54 +0000

    Correct MATLAB and Python paths in fish APS_CONFIG

--- APS_CONFIG.fish
+++ APS_CONFIG.fish
@@ -2,5 +2,5 @@
 set -x APS_toolbox "$HOME/dev/matlab/TRAIN"
 set -x APS_toolbox_scripts "$APS_toolbox/scripts"
 set -x APS_toolbox_bin "$APS_toolbox/bin"
-set -x PYTHONPATH "$HOME/dev/matlab/TRAIN/python_modules" "$PYTHONPATH"
-set -x MATLABPATH "$APS_toolbox/matlab" "$MATLABPATH"
+set -x PYTHONPATH "$HOME/dev/matlab/TRAIN/python_modules:$PYTHONPATH"
+set -x MATLABPATH "$APS_toolbox/matlab:$MATLABPATH"
